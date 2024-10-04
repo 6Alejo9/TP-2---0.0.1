0801343dc8148c906b04adb65f77804cd6920ccb
Alejo Ojeda <alejoojedakirby@gmail.com>
2024-10-04 16:42:29 -0300

    si

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtConstraseña = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnRecupero = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(38, 31);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(54, 16);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Location = new System.Drawing.Point(38, 47);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(76, 16);
+            this.lblContraseña.TabIndex = 1;
+            this.lblContraseña.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(157, 31);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 22);
+            this.txtUsuario.TabIndex = 4;
+            // 
+            // txtConstraseña
+            // 
+            this.txtConstraseña.Location = new System.Drawing.Point(157, 59);
+            this.txtConstraseña.Name = "txtConstraseña";
+            this.txtConstraseña.Size = new System.Drawing.Size(100, 22);
+            this.txtConstraseña.TabIndex = 5;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(115, 151);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(167, 40);
+            this.btnAceptar.TabIndex = 6;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnRecupero
+            // 
+            this.btnRecupero.Location = new System.Drawing.Point(115, 197);
+            this.btnRecupero.Name = "btnRecupero";
+            this.btnRecupero.Size = new System.Drawing.Size(167, 41);
+            this.btnRecupero.TabIndex = 7;
+            this.btnRecupero.Text = "recuperar contraseña";
+            this.btnRecupero.UseVisualStyleBackColor = true;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(115, 244);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(167, 41);
+            this.btnRegister.TabIndex = 8;
+            this.btnRegister.Text = "Registrase";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(415, 450);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnRecupero);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtConstraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblContraseña);
+            this.Controls.Add(this.lblUsuario);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtConstraseña;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnRecupero;
+        private System.Windows.Forms.Button btnRegister;
     }
 }
 
