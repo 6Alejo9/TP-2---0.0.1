1b945e1e9b752c7b7f4e504cc8939570dd8db9b0
Alejo Ojeda <alejoojedakirby@gmail.com>
2024-10-04 19:44:34 -0300

    otro cambio

--- forms/Registrar Usuario.Designer.cs
+++ forms/Registrar Usuario.Designer.cs
@@ -31,6 +31,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnAceptarRegistro = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -46,7 +51,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(11, 76);
+            this.label2.Location = new System.Drawing.Point(11, 88);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(91, 13);
@@ -63,15 +68,59 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "Contraseña";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(170, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(170, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // btnAceptarRegistro
+            // 
+            this.btnAceptarRegistro.Location = new System.Drawing.Point(123, 195);
+            this.btnAceptarRegistro.Name = "btnAceptarRegistro";
+            this.btnAceptarRegistro.Size = new System.Drawing.Size(75, 42);
+            this.btnAceptarRegistro.TabIndex = 7;
+            this.btnAceptarRegistro.Text = "Aceptar";
+            this.btnAceptarRegistro.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 243);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 42);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "¿ Ya Estas Registrado ?";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(309, 366);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAceptarRegistro);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Register";
             this.Text = "Form2";
             this.ResumeLayout(false);
@@ -84,5 +133,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnAceptarRegistro;
+        private System.Windows.Forms.Button button2;
     }
 }
