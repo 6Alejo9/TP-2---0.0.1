0cb018bddb999e64961fa9950531e6a51837ecdd
Alejo Ojeda <alejoojedakirby@gmail.com>
2024-10-18 17:45:57 -0300

    Metodo Coneccion con BD

---
+++ Logic/Metodo Conectar BD.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_2___0._0._1.Logic
+{
+    public class DatabaseConnection
+    {
+        private string connectionString;
+
+        // Constructor: Define la cadena de conexión al inicializar el objeto
+        public DatabaseConnection()
+        {
+            connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Alejo\Desktop\Tareas y Otros\Algoritmos y estructura de datos 2\TP 2\TP 2 0.0.1\BD_TP_grupal_parte_2.accdb";
+        }
+
+        // Método para obtener la conexión a la base de datos
+        public OleDbConnection Connect()
+        {
+            try
+            {
+                OleDbConnection connection = new OleDbConnection(connectionString);
+                connection.Open();
+                Console.WriteLine("Conexión exitosa.");
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al conectar con la base de datos: " + ex.Message);
+                return null;
+            }
+        }
+
+        // Método para cerrar la conexión
+        public void CloseConnection(OleDbConnection connection)
+        {
+            if (connection != null && connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+                Console.WriteLine("Conexión cerrada.");
+            }
+        }
+    }
+
+
+
+}
+
--- forms/Recupero Contraseña.cs
+++ forms/Recupero Contraseña.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TP_2___0._0._1.Logic;
 
 namespace TP_2___0._0._1.forms
 {
@@ -24,10 +25,19 @@ namespace TP_2___0._0._1.forms
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            //Llamar al metodo dedicado a conectar la base de datos
+            DatabaseConnection DBConnection = new DatabaseConnection ();
+          
+                // Crear la consulta SQL para validar el usuario y la contraseña
+            string query = "SELECT COUNT(*) FROM Usuarios WHERE Correo_Electronico = @email";
         }
 
         private void frmRecupero_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
